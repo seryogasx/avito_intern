04a2b47fc9bf1b889cbf4629f08237fb05b95dad
seryogasx <sergio.petrovx@yandex.ru>
2021-09-07 12:51:28 +0300

    add Model file

---
+++ avito_intern/Model.swift
@@ -0,0 +1,19 @@
+//
+//  Model.swift
+//  avito_intern
+//
+//  Created by Сергей Петров on 07.09.2021.
+//
+
+import Foundation
+
+struct Company: Codable {
+    var name: String
+    var employees: [Employee]
+}
+
+struct Employee: Codable {
+    var name: String
+    var phone_number: String
+    var skills: [String]
+}
